22986b982011f98e56dd13349897d9f513b4575e
Wesley Jacobs <wesleyjacobs@Wesleys-MacBook-Pro.local>
2019-10-14 09:52:34 -0700

    added boilerplate for components

---
+++ src/BookList/bookItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class BookItem extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default BookItem;
---
+++ src/SearchBar/bookType.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class BookType extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default BookType;
